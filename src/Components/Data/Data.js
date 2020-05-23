9f0150cb98e2458753c9eda83cf9b3f8ac4c278a
Sumit <sumit.sth54@gmail.com>
2020-05-23 16:35:39 +0545

    added more styles

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import './Data.css'
 
 const Data = ({active,recovered,death}) =>{
     return(
-        <div>
-            <ul>
-            <li>Total Cases: {active+recovered+death}</li>
-            <li>Active Cases: {active}</li>
-            <li>Recovered Cases: {recovered}</li>
-            <li>Deaths: {death}</li>
-            </ul>
+        <div className='data-grid'>
+            <div className='data-box box1'>Total Cases <br/>{active+recovered+death}</div>
+            <div className='data-box box2'>Active Cases<br/> {active}</div>
+            <div className='data-box box3'>Recovered Cases<br/> {recovered}</div>
+            <div className='data-box box4'>Deaths<br/> {death}</div>
         </div>
     )
 }
