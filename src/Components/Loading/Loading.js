905487fc53c172178c6e238d3980cfd0d1ba0c9f
Sumit <sumit.sth54@gmail.com>
2020-05-29 08:59:16 +0545

    Added loading screen designed by Saroj

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './Loading.css';
+import coronavirus from '../Navigation/coronavirus.svg';
 
 const Loading = () => {
     return(
-        <h1>Loading...</h1>
+        <div class="loading-front">                  
+            <div class="loading-text">
+                loading
+                <p class="dot" id="d1">.</p>
+                <p class="dot" id="d2">.</p>
+                <p class="dot" id="d3">.</p>
+            </div>                                        
+            <div class="loading-box"><img src={coronavirus} alt='covid19' /></div> 
+        </div> 
     )
 }
 
