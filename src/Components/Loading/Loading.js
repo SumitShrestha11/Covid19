cc91a1e6cbc9f207cb01b3980ed279fef166321e
Sumit <sumit.sth54@gmail.com>
2020-05-29 09:28:38 +0545

    Added Loading screen designed by saroj

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -4,14 +4,14 @@ import coronavirus from '../Navigation/coronavirus.svg';
 
 const Loading = () => {
     return(
-        <div class="loading-front">                  
-            <div class="loading-text">
+        <div className="loading-front">                  
+            <div className="loading-text">
                 loading
-                <p class="dot" id="d1">.</p>
-                <p class="dot" id="d2">.</p>
-                <p class="dot" id="d3">.</p>
+                <p className="dot" id="d1">.</p>
+                <p className="dot" id="d2">.</p>
+                <p className="dot" id="d3">.</p>
             </div>                                        
-            <div class="loading-box"><img src={coronavirus} alt='covid19' /></div> 
+            <div className="loading-box"><img src={coronavirus} alt='covid19' /></div> 
         </div> 
     )
 }
